ace219a9f59a5d3f6f799856b95bb72697e57cb2
pinjster <pinj2000@gmail.com>
2023-10-16 22:09:17 -0400

    added singup inputs

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 export default function signupPage() {
   return (
-    <div>signupPage</div>
+    <div>
+      <form action="">
+        <input type="text" name="username" id="" />
+        <input type="text" name="password" id="" />
+      </form>
+    </div>
   )
 }
 
